eb376e41297e099ffba22757fef818508ff34276
AllEyesOnJose <stanekpepa@gmail.com>
2025-01-18 22:57:43 +0100

    uvidime

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import * as THREE from "./node_modules/three/build/three.module.js";
-import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
-import { TextureLoader } from "./node_modules/three/build/three.module.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TextureLoader } from "three";
+
 // Vytvoreni sceny
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
